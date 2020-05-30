a54842984c482d71b8eb3e9e479b2f94028303cb
Nicholas Mullen <nwmullen@gmail.com>
2020-05-29 21:26:45 -0400

    add migration for intial table creation

---
+++ data/migrations/20200529210930_create-initial-tables.js
@@ -0,0 +1,54 @@
+
+exports.up = function(knex) {
+  return (
+    knex.schema
+      .createTable('project', tbl => {
+        tbl.increments();
+        tbl.string('name', 255).notNullable();
+        tbl.string('description', 255);
+        tbl.boolean('completed').defaultTo(false).notNullable();
+      })
+      .createTable('resource', tbl => {
+        tbl.increments();
+        tbl.string('name', 255).notNullable().unique();
+        tbl.string('description', 255);
+      })
+      .createTable('task', tbl => {
+        tbl.increments();
+        tbl.string('description', 255).notNullable();
+        tbl.text('notes', 255);
+        tbl.boolean('completed').defaultTo(false).notNullable();
+        tbl.integer('project_id', 255)
+          .unsigned()
+          .notNullable()
+          .references('project.id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+      })
+      .createTable('project_resource', tbl => {
+        tbl.increments();
+        tbl.integer('project_id', 255)
+          .unsigned()
+          .notNullable()
+          .references('project.id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+        tbl.integer('resource_id', 255)
+          .unsigned()
+          .notNullable()
+          .references('resource.id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+      })
+  );
+};
+
+exports.down = function(knex) {
+  return (
+    knex.schema
+      .dropTableIfExists('project')
+      .dropTableIfExists('resource')
+      .dropTableIfExists('task')
+      .dropTableIfExists('project_resource')
+  );
+};
