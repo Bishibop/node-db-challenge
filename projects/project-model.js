3f25e97ee28a73b0036440d3549c4b75d99ace78
Nicholas Mullen <nwmullen@gmail.com>
2020-05-29 23:22:24 -0400

    stretch

--- projects/project-model.js
+++ projects/project-model.js
@@ -5,8 +5,19 @@ async function find() {
 }
 
 async function findById(id) {
-  return db('project').where({id: id}).first();
-  // add a join here for the resource names
+  // STRETCH
+  let project = await db('project').where({id: id}).first();
+  project.tasks = await db('task AS t')
+    .where({project_id: id})
+    .select('t.id', 't.description', 't.notes', 't.completed');
+  project.resources = await db('project_resource AS pr')
+    .where({project_id: id})
+    .join('resource AS r', 'pr.resource_id', 'r.id')
+    .select('r.id', 'r.name', 'r.description');
+  [project, ...project.tasks].forEach(el => {
+    el.completed = el.completed ? true : false;
+  })
+  return project;
 }
 
 async function add(projectData) {
--- projects/project-router.js
+++ projects/project-router.js
@@ -14,6 +14,23 @@ router.get('/', (req, res) => {
   });
 });
 
+// STRETCH
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Projects.findById(id)
+  .then(project => {
+    if (project) {
+      res.json(project);
+    } else {
+      res.status(404).json({ message: 'Could not find project with given id.' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get project' });
+  });
+});
+
 router.post('/', (req, res) => {
   const projectData = req.body;
   Projects.add(projectData)
